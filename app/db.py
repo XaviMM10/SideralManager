f8bc2c34f3f0ff2f299c4c479eb804d1698884e0
XaviMM10 <javier.millan1@estudiant.uib.eu>
2026-06-15 20:57:30 +0200

    Debugging

--- app/db.py
+++ app/db.py
@@ -1,11 +1,19 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.engine.url import make_url
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://office_user:0000@localhost:5432/office_db"
 )
+
+parsed_url = make_url(DATABASE_URL)
+
+print("DB DEBUG USER:", parsed_url.username)
+print("DB DEBUG HOST:", parsed_url.host)
+print("DB DEBUG PORT:", parsed_url.port)
+print("DB DEBUG DATABASE:", parsed_url.database)
 
 engine = create_engine(DATABASE_URL)
 
